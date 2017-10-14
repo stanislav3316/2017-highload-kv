2789b062135706a8d0356aefa332a24ebd3803d6
kerneq <stasik-0@yandex.ru>
2017-10-14 22:13:45 +0300

    add kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 // See https://gradle.org and https://github.com/gradle/kotlin-dsl
 
 // Apply the java plugin to add support for Java
-/*plugins {
+plugins {
     java
     application
-}*/
-apply plugin: 'java'
+}
 
 repositories {
     jcenter()
@@ -13,30 +12,28 @@ repositories {
 
 dependencies {
     // Annotations for better code documentation
-    compile 'com.intellij:annotations:12.0'
-
-    compile 'com.googlecode.json-simple:json-simple:1.1'
+    compile("com.intellij:annotations:12.0")
 
     // JUnit test framework
-    testCompile 'junit:junit:4.12'
+    testCompile("junit:junit:4.12")
 
     // HTTP client for unit tests
-    testCompile 'org.apache.httpcomponents:fluent-hc:4.5.3'
+    testCompile("org.apache.httpcomponents:fluent-hc:4.5.3")
+
+    // Guava for tests
+    testCompile("com.google.guava:guava:23.1-jre")
 }
 
-/*
 tasks {
     "test"(Test::class) {
         maxHeapSize = "1g"
     }
 }
-*/
 
-
-/*application {
+application {
     // Define the main class for the application
     mainClassName = "ru.mail.polis.Server"
 
     // And limit Xmx
     applicationDefaultJvmArgs = listOf("-Xmx1g")
-}*/
+}
